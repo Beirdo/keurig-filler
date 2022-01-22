2dc47d5edad4a8351e6c26b85a0a1c9cb4e96c94
Gavin Hurlbut <gjhurlbu@gmail.com>
2022-01-22 01:21:30 -0800

    Complete overhaul of water sensor code
    - now doing 8 measurements, and averaging the results before conversion
    - conversion now calculates the resistance (much like with a thermistor), and then uses exponential regression to get the percentage of fullscale reading.

--- firmware/main.py
+++ firmware/main.py
@@ -1,6 +1,6 @@
 import time
-from enum import Enum
-from threading import Lock
+import math
+import _thread
 
 from machine import Pin, Timer, ADC
 
@@ -13,35 +13,60 @@ Basic Flow:
     3) On interrupt from the flow sensor, increment a counter, which we reset every second in the timer handler
 
 Timer Handler:
-    1) Read ADC2 to get a ground offset
-    2) Read ADC0 to get a level reading at the Keurig's reservoir
-    3) Read ADC1 to get a level reading at the 5gal source jug
-    4) Read out the flow counter and reset to zero
-    5) Update pump control state-machine
-    6) Send update out UART (over USB)
+    1) Read ADC0 to get a level reading at the Keurig's reservoir
+    2) Read ADC1 to get a level reading at the 5gal source jug
+    3) Read out the flow counter and reset to zero
+    4) Update pump control state-machine
+    5) Send update out UART (over USB)
 """
 
 
-class PumpStates(Enum):
-    IDLE = 1
-    PUMP = 2
-    STOP = 3
-    NO_WATER = 4
-    RESET = 5
-
+class PumpStates(object):
+    states = {
+        1: "IDLE",
+        2: "PUMP",
+        3: "STOP",
+        4: "NO_WATER",
+        5: "RESET",
+    }
+ 
+    states_by_name = {value: key for (key, value) in states.items()}
+    
+    def __init__(self, state=None, value=None):
+        if state is not None:
+            value = getattr(self, state)
+            
+        if value is not None:
+            if value in self.states.keys():
+                self.value = value
+            else:
+                raise KeyError("No state maps out to value %s" % value)
+    
+    def __getattr__(self, state):
+        if state == "name":
+            attr = self.states.get(self.value, None)
+            if not attr:
+                raise KeyError("State with value %s does not exist" % self.value)
+            return attr
+            
+        attr = self.states_by_name.get(state.upper(), None)
+        if not attr:
+            raise KeyError("State %s does not exist" % state)
+        return attr
+    
 
 class StateMachine(object):
     def __init__(self):
-        self.state = PumpStates.IDLE
+        self.state = PumpStates("IDLE")
 
     def transition(self, to_state: PumpStates) -> PumpStates:
         if not isinstance(to_state, PumpStates):
-            to_state = PumpStates.RESET
+            to_state = PumpStates().RESET
             funcname = "_reset"
         else:
-            funcname = "_from_%s_to_%s" % (self.state.name, to_state.name)
+            funcname = "_from_%s_to_%s" % (self.state.name, PumpStates(value=to_state).name)
 
-        print("New State: %s" % to_state.name)
+        print("New State: %s" % PumpStates(value=to_state).name)
 
         funcname = funcname.lower()
 
@@ -56,40 +81,104 @@ class StateMachine(object):
         return self.state
 
     def _reset(self) -> PumpStates:
-        if self.state == PumpStates.PUMP:
-            return self.transition(PumpStates.STOP)
+        if self.state == PumpStates().PUMP:
+            return self.transition(PumpStates().STOP)
         print("New State: IDLE")
-        return PumpStates.IDLE
+        return PumpStates().IDLE
 
     def _from_pump_to_stop(self) -> PumpStates:
         global pump_en
         pump_en.value(0)
-        return self.transition(PumpStates.IDLE)
+        return self.transition(PumpStates().IDLE)
 
     def _from_pump_to_no_water(self) -> PumpStates:
-        return self.transition(PumpStates.STOP)
+        return self.transition(PumpStates().STOP)
 
     def _from_idle_to_pump(self) -> PumpStates:
         global pump_en
         pump_en.value(1)
-        return PumpStates.PUMP
+        return PumpStates().PUMP
 
 
-def scale(value, min_value, max_value, factor=1.0, offset=0.0):
-    # clamp value between min_value and max_value
-    value = min(max_value, max(min_value, value))
+scale_a = math.log(2.053)
+scale_b = math.log(0.9989)
 
-    # Translate to 0-based
-    value -= min_value
 
-    # Apply the factor
-    value *= factor
+def scale_liquid_level(value):
+    print("%03X" % value)
+    
+    '''
+    First we need to calculate the resistance, much like one would with a
+    thermistor.   We create a resistive voltage divider with a resistor going
+    from the ADC input to ground.  I have chosen a 1k resistor as it seemed to
+    be relatively similar to much of the range.
+    
+    In the end the resistance vs level graph is an exponential decay that looks
+    very much like the ones for NTD thermistors
+    '''
+    r = 1000
+    vin = 3.3
+    vo = 3.3 * value / 4096
+    r_sensor = r * ((vin / vo) - 1)
+    print("%0.3f ohm" % r_sensor)
 
-    # Apply the offset
-    value += offset
+    '''
+    Now, let's calibrate this (using emperically determined values).  At 100% we
+    see around 700 ohms, and at 1% (just starting to trigger) it's around 4k ohms.
+    When the sensor is completely dry, it gets to around 100k ohms.  We'll cap the
+    measured value
+    '''
+    sensor_min_r = 700
+    sensor_max_r = 4000
+    sensor_r_range = sensor_max_r - sensor_min_r
+    
+    r_sensor = min(sensor_max_r, max(sensor_min_r, r_sensor))
+    print("%0.3f ohm" % r_sensor)
+    
+    '''
+    We are going to use exponential regression (two layers deep) to find
+    the % full based on:
+        x       y = R
+        0       4000
+        25      1150
+        50      800
+        75      720
+        100     700
+        
+    After the first logarithm:
+        x        y1 = ln(y)
+        0        8.294049
+        25       7.047517
+        50       6.684612
+        75       6.579251
+        100      6.55108
+        
+    This is still clearly somewhat exponential.
+    
+    After the second logarithm:
+        x        y2 = ln(y1)
+        0        2.115538
+        25       1.952675
+        50       1.899808
+        75       1.883921
+        100      1.87963
+        
+    This maps out to:
+       y2 = a * b^x
+       with a = 2.053, b = 0.9989
+         
+       y2 = 2.053 * 0.9989^x
+    
+        x = (ln(y2) - ln(a)) / ln(b)
+    '''
 
-    return value
-
+    y1 = math.log(r_sensor)
+    y2 = math.log(y1)
+    y3 = math.log(y2)
+    percent_value = max(0, (y3 - scale_a) / scale_b)
+    print("Scaled %%: %0.3f" % percent_value)
+    return percent_value
+    
 
 def pulse_irq(pin):
     global flow_count
@@ -97,7 +186,7 @@ def pulse_irq(pin):
         flow_count += 1
 
 
-flow_mutex = Lock()
+flow_mutex = _thread.allocate_lock()
 
 pump_en = Pin(22, Pin.OUT)
 pump_en.value(0)
@@ -107,17 +196,10 @@ flow_count = 0
 flow_pulse = Pin(20, Pin.IN, Pin.PULL_DOWN)
 flow_pulse.irq(pulse_irq, Pin.IRQ_RISING)
 
-adcs = [ADC(i) for i in range(3)]
-
-# These will need calibrating
-level_sensor_min = 2000
-level_sensor_max = 4095
 flow_factor = 1.0 / 740
 
-# We want this in % level on the sensor
-level_sensor_scale = 100.0 * (level_sensor_max - level_sensor_min)
-
 pump_state = StateMachine()
+loop_counter = 0
 
 while True:
     with flow_mutex:
@@ -126,26 +208,34 @@ while True:
 
     flow = counter * flow_factor
 
-    # MicroPython has some stupid method of expanding to 16 bit values which is not linear.  We will use the
-    # original 12bit ADC readings, thanks.
-    readings = [adc.read_u16() >> 4 for adc in adcs]
-    keurig_level = scale(readings[0] - readings[2], level_sensor_min, level_sensor_max, level_sensor_scale)
-    source_level = scale(readings[1] - readings[2], level_sensor_min, level_sensor_max, level_sensor_scale)
+    loop_counter += 1
 
-    if pump_state.state == PumpStates.PUMP and flow == 0.0:
+    '''
+    MicroPython has some stupid method of expanding to 16 bit values which is not linear.  We will use the
+    original 12bit ADC readings, thanks.  Also, since these sensors are finnicky, average over 8 values.
+    '''
+    keurig_levels = [ADC(0).read_u16() for i in range(8)]
+    print("Keurig: %s" % list(map(lambda x: "%04X" % x, keurig_levels)))
+    keurig_level = scale_liquid_level(sum(keurig_levels) >> 7)
+    
+    source_levels = [ADC(1).read_u16() for i in range(8)]
+    print("Source: %s" % list(map(lambda x: "%04X" % x, source_levels)))
+    source_level = scale_liquid_level(sum(source_levels) >> 7)
+
+    if pump_state.state == PumpStates().PUMP and flow == 0.0:
         # The pump is on, but no water flowing?!  Shut it off!
-        pump_state.transition(PumpStates.STOP)
+        pump_state.transition(PumpStates().STOP)
     elif source_level < 10.0:
         # We are almost out of water.  Stop pumping if we are, and don't start until there's water
-        pump_state.transition(PumpStates.NO_WATER)
+        pump_state.transition(PumpStates().NO_WATER)
     elif keurig_level < 5.0:
         # We have source water, and the Keurig is low on water.  Start pumping!
-        pump_state.transition(PumpStates.PUMP)
+        pump_state.transition(PumpStates().PUMP)
     elif keurig_level > 75.0:
         # The Keurig is getting full enough, we can stop now.
-        pump_state.transition(PumpStates.STOP)
+        pump_state.transition(PumpStates().STOP)
 
-    print("State: %s, Keurig Level: %0.2f, Source Level %0.2f, Flow: %0.3f" %
-          (pump_state.state.name, keurig_level, source_level, flow))
+    print("%s: State: %s, Keurig Level: %0.2f, Source Level %0.2f, Flow: %0.3f" %
+          (loop_counter, PumpStates(value=pump_state.state).name, keurig_level, source_level, flow))
 
     time.sleep(1.0)
